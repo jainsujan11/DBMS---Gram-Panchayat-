4925dd446cf492193d393d94910f3a2b69b0f7d7
ashish132003 <www.phy2003@gmail.com>
2025-03-01 23:51:42 +0530

    Government Monitor can add welfare scheme, see :birth_rate,literacy_rate,assets

--- app/routes/govt.py
+++ app/routes/govt.py
@@ -1,5 +1,5 @@
 # app/govt.py
-from flask import Blueprint, render_template, session, flash, redirect, url_for
+from flask import Blueprint, render_template, session, flash, redirect, url_for,request
 from connect import get_db, close_db  # Fix import
 
 govt_bp = Blueprint('govt', __name__)
@@ -24,3 +24,44 @@ def govt_dashboard():
 
     stats = {'total_rice': total_rice, 'total_citizens': total_citizens}
     return render_template('govt.html', stats=stats)
+
+#respond to GET requests , accept POST requests
+@govt_bp.route('/add_scheme',methods=['GET','POST'])
+def govt_add_scheme():
+    if request.method == 'GET':
+        #first render html page to get response
+        return render_template('govt_add_scheme.html')
+    else:   
+        #here we get response
+        scheme_number = request.form.get('scheme_number')
+        scheme_name = request.form.get('scheme_name')
+        scheme_description = request.form.get('scheme_description')
+
+        conn = get_db()
+        cur = conn.cursor()
+
+        cur.execute("insert into welfare_schemes values (%s,%s,%s)",(scheme_number,scheme_name,scheme_description))
+        conn.commit() #important
+        conn.close()
+        flash("welfare scheme added successfully.")
+
+        return redirect(url_for('govt.govt_add_scheme'))
+
+@govt_bp.route('/print_statistics',methods=['POST'])
+def govt_print_statistics():
+    which_query=request.form.get('which_query')
+    query=None
+    if(which_query=='Birth_Rate'):
+        query="select * from birth_rate_2001"  #select gender,count(*) from citizens where EXTRACT(YEAR FROM dob) = 2001 group by gender
+    elif(which_query=='Literacy_Rate'):
+        query="select * from literacy_rate" #select * from citizens where educational_qualification != 'Secondary';
+    else:
+        query="select * from assets"
+
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute(query)
+    results = cur.fetchall()
+
+    conn.close()
+    return render_template('employee_query_result.html',query_type=which_query,results=results)

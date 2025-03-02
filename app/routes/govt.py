77e61aaf281c5087912c14f199d48a0738f6b81e
ashish132003 <www.phy2003@gmail.com>
2025-03-02 19:41:37 +0530

    try_catch block to catch error when inserting schemes. COMPLETE

--- app/routes/govt.py
+++ app/routes/govt.py
@@ -9,21 +9,7 @@ def govt_dashboard():
     if 'user_type' not in session or session['user_type'] != 'govt':
         flash("Unauthorized access")
         return redirect(url_for('auth.login'))
-
-    conn = get_db()
-    cur = conn.cursor()
-
-    cur.execute("SELECT SUM(area_acres) FROM land_records WHERE crop_type = 'Rice'")
-    total_rice = cur.fetchone()[0]  # Fetch as tuple
-
-    cur.execute("SELECT COUNT(*) FROM citizens")
-    total_citizens = cur.fetchone()[0]  # Fetch as tuple
-
-    cur.close()
-    close_db()
-
-    stats = {'total_rice': total_rice, 'total_citizens': total_citizens}
-    return render_template('govt.html', stats=stats)
+    return render_template('govt.html')
 
 #respond to GET requests , accept POST requests
 @govt_bp.route('/add_scheme',methods=['GET','POST'])
@@ -39,11 +25,16 @@ def govt_add_scheme():
 
         conn = get_db()
         cur = conn.cursor()
-
-        cur.execute("insert into welfare_schemes values (%s,%s,%s)",(scheme_number,scheme_name,scheme_description))
-        conn.commit() #important
-        conn.close()
-        flash("welfare scheme added successfully.")
+        
+        try:
+            cur.execute("insert into welfare_schemes values (%s,%s,%s)",(scheme_number,scheme_name,scheme_description))
+            conn.commit() #important
+            conn.close()
+            flash("welfare scheme added successfully.")
+        except Exception as e:
+            flash("Welface scheme addition failed")
+            flash(f"Error: {e}")
+            conn.close()
 
         return redirect(url_for('govt.govt_add_scheme'))
 
@@ -60,7 +51,7 @@ def govt_print_statistics():
     elif(which_query == 'Assets'):
         query="select * from assets"
     elif(which_query == 'Poverty Line'):
-        query="select 'BELOW POVERTY LINE',count(*) as count from households where income < (select avg(income) from households)" 
+        query="select 'BELOW POVERTY LINE' as Stats,count(*) as count from households where income < (select avg(income) from households)" 
     elif(which_query =='Scheme Enrollments'):
         query="select scheme_id,count(*) as count from scheme_enrollments group by scheme_id"
 
@@ -69,9 +60,11 @@ def govt_print_statistics():
     cur.execute(query)
     results = cur.fetchall()
 
-    print(results)
+    column_names = [desc[0] for desc in cur.description]
+
+    print(results,column_names)
 
     conn.close()
     if(which_query=='Birth_Rate'):
         return render_template('display_graph.html',data=results)
-    return render_template('employee_query_result.html',query_type=which_query,results=results)
+    return render_template('employee_query_result.html',query_type=which_query,results=results,column_names=column_names)
